3dcfa89ac66422066ed24b41b01d32ed1b9c0fb0
BryceY <nash31@korea.ac.kr>
2020-05-29 13:59:56 +0900

    프로그래머스 49993 스킬트리 문제풀이

--- Algorithms_Practice/programmers/스킬트리_49993.py
+++ Algorithms_Practice/programmers/스킬트리_49993.py
@@ -1,5 +1,18 @@
 '''
 https://programmers.co.kr/learn/courses/30/lessons/49993
-
 '''
 
+def solution(skill, skill_trees):
+    answer = 0
+
+    for s in skill_trees:
+        tmp_skill = ""
+        for char in s:
+            if char in skill:
+                tmp_skill += char
+        if skill[:len(tmp_skill)] == tmp_skill:
+            answer += 1
+    return answer
+
+
+print(solution("CBD",["BACDE", "CBADF", "AECB", "BDA"]))
