93854b39620d08f18c31fb62e3b4e71caa1cf390
BryceY <nash31@korea.ac.kr>
2020-05-29 17:33:29 +0900

    62049 문제풀이

--- Algorithms_Practice/programmers/종이접기_62049.py
+++ Algorithms_Practice/programmers/종이접기_62049.py
@@ -7,11 +7,15 @@ def solution(n):
     if n == 1:
         return [0]
     else:
-        left = solution(n-1).append(0)
+        left = list()
+        right = list()
+        left = solution(n-1)
+        mid = [0]
         right = solution(n-1)
         right[len(right)//2] = 1
+        left.extend(mid)
         left.extend(right)
         return left
 
 
-print(solution(2))
+print(solution(3))
