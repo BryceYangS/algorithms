b14363f80c9569eb2a6b5664d08e72cebc8b6bb9
BryceY <nash31@korea.ac.kr>
2020-06-03 12:57:39 +0900

    프로그래머스 42584

---
+++ Algorithms_Practice/programmers/스택_큐_주식가격_42584.py
@@ -0,0 +1,17 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/42584
+'''
+
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices) - 1):
+        for j in range(i+1, len(prices)):
+            answer[i] += 1
+            if prices[i] > prices[j]:
+                break
+
+    return answer
+
+
+print(solution([1,2,3,2,3])) #[4, 3, 1, 1, 0]
