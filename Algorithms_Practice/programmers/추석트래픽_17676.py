3cbe6e2b6ee6b0602c21a79419a06e10f4db3963
Bryce <nash31@korea.ac.kr>
2020-06-08 21:42:43 +0900

    추석트래픽

--- Algorithms_Practice/programmers/추석트래픽_17676.py
+++ Algorithms_Practice/programmers/추석트래픽_17676.py
@@ -1,6 +1,8 @@
 '''
 https://programmers.co.kr/learn/courses/30/lessons/17676?language=python3
 '''
+
+'''
 import math
 
 def solution(lines):
@@ -33,6 +35,52 @@ def solution(lines):
 def convertToTrStTimeSnds(end_time, pro_time):
     tr_st_time = end_time + 0.001 - (float)(pro_time)
     return tr_st_time
+'''
+
+from decimal import Decimal
+def solution(lines):
+    answer = 1
+
+    tmp = [(
+        (int)(i.split(' ')[1].split(':')[0])*3600 + (int)(i.split(' ')[1].split(':')[1])*60 + Decimal(i.split(' ')[1].split(':')[2]) + Decimal('0.001') - Decimal(i.split(' ')[2][:-1]),
+            (int)(i.split(' ')[1].split(':')[0])*3600 + (int)(i.split(' ')[1].split(':')[1])*60 + Decimal(i.split(' ')[1].split(':')[2])
+    ) for i in lines] #(start, end)
+    print(tmp)
+
+    for log1 in tmp:
+        chkSt = log1[0] - Decimal('0.0001')
+        chkEd = log1[0] + 1
+        # chkSt = log1[0]
+        # chkEd = log1[0] + Decimal('0.9999')
+
+        maxProcess = 0
+        for log2 in tmp:
+            if log2[0] < chkEd and log2[1] > chkSt:
+                maxProcess += 1
+            # elif chkSt < log2[1] < chkEd:
+            #     maxProcess += 1
+        answer = maxProcess if answer < maxProcess else answer
+    print(answer)
+
+    # for log1 in tmp:
+    #     chkSt = log1[1] - Decimal('0.001')
+    #     chkEd = log1[1] + 1
+    #     # chkSt = log1[0]
+    #     # chkEd = log1[0] + Decimal('0.9999')
+    #
+    #     maxProcess = 0
+    #     for log2 in tmp:
+    #         if log2[0] < chkEd and log2[1] > chkSt:
+    #             maxProcess += 1
+    #         # elif chkSt < log2[1] < chkEd:
+    #         #     maxProcess += 1
+    #
+    #     answer  = maxProcess if answer < maxProcess else answer
+
+    return answer
+    # print(answer)
+
+
 
 # solution([
 # '2016-09-15 01:00:04.001 2.0s',
@@ -41,7 +89,7 @@ def convertToTrStTimeSnds(end_time, pro_time):
 
 # solution(["2016-09-15 00:00:00.000 3s"])
 # solution(["2016-09-15 23:59:59.999 0.001s"])
-# solution(["2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"])
+solution(["2016-09-15 01:00:04.001 2.0s", "2016-09-15 01:00:07.000 2s"])
 # solution(["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"])
-solution(["2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s", "2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s"])
+# solution(["2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s", "2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s"])
 # solution(["2016-09-15 00:00:00.000 2.3s", "2016-09-15 23:59:59.999 0.1s"])
--- Algorithms_Practice/test.py
+++ Algorithms_Practice/test.py
@@ -1,13 +1,17 @@
-class Quardangle:
-    def __init__(self, width, height, color):
-        self.width = width
-        self.height = height
-        self.color = color
+# class Quardangle:
+#     def __init__(self, width, height, color):
+#         self.width = width
+#         self.height = height
+#         self.color = color
+#
+#     def __del__(self):
+#         print('Quardangle object is deleted')
+#
+# s = Quardangle(1,1,'aa')
+# print(s.width)
+# del s
+# print(s)
 
-    def __del__(self):
-        print('Quardangle object is deleted')
+import decimal
 
-s = Quardangle(1,1,'aa')
-print(s.width)
-del s
-print(s)
+print(decimal.Decimal('0.2') + decimal.Decimal('0.1'))
