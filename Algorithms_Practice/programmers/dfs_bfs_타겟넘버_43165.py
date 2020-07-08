289c93ba4fb75dfccb816f9e534de6aa72aaf654
Bryce <nash31@korea.ac.kr>
2020-07-09 00:42:00 +0900

    update

--- Algorithms_Practice/programmers/dfs_bfs_타겟넘버_43165.py
+++ Algorithms_Practice/programmers/dfs_bfs_타겟넘버_43165.py
@@ -5,8 +5,16 @@ https://programmers.co.kr/learn/courses/30/lessons/43165
 def solution(numbers, target):
     answer = 0
 
+    def dfs(numbers, target, sum, idx):
+        if idx == len(numbers):
+            if sum == target :
+                nonlocal answer
+                answer += 1
+            return
+        dfs(numbers, target, sum + numbers[idx], idx+1)
+        dfs(numbers, target, sum - numbers[idx], idx+1)
 
-
+    dfs(numbers, target, 0, 0)
 
     return answer
 
--- Algorithms_Practice/test.py
+++ Algorithms_Practice/test.py
@@ -12,6 +12,24 @@
 # del s
 # print(s)
 
-import decimal
+# import decimal
+#
+# print(decimal.Decimal('0.2') + decimal.Decimal('0.1'))
 
-print(decimal.Decimal('0.2') + decimal.Decimal('0.1'))
+from functools import reduce
+print(reduce(lambda x,y: x + y, [0,1,2,3,4]))
+print(reduce(lambda x,y : y + x, 'abcde'))
+
+# filter(함수, 리스트)
+print(list(filter(lambda x: x < 5, [0,1,2,3,4,5,6])))
+
+a = [-3,-2,-1,0,1,2,3,4]
+
+print( 5 % 2 )
+print(list(filter(lambda x: x < 0, a)))
+print(list(filter(lambda x: x % 2, range(10))))
+
+
+# map
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list(map(lambda x: str(x) if x % 3 == 0 else x, a)))
